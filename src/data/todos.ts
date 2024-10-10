286b482ca9df0053a7a385eec64e9de65e7bd47b
Marianna Mazzette <20104120@mail.wit.ie>
2024-10-10 19:36:02 +0100

    added interface and dummy data

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todos";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: 'Learn React 🚀',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Learn TypeScript 🚀',
+    completed: false
+  },
+  {
+    id: 3,
+    title: 'Learn GraphQL 🎉',
+    completed: false
+  },
+];
