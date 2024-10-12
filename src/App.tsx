b1c51cc552ecf81176fca1920976aaf2f403982c
Marianna Mazzette <20104120@mail.wit.ie>
2024-10-12 22:11:14 +0100

    added setToDoCompleted function and callback props

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
 
 function App() {
+
+  function setTodoCompleted(id: number, completed: boolean){
+    alert(`Todo with id ${id} is now ${completed ? "completed" : "not completed"}`)
+  }
   return (
     <main className="py-10 h-screen">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="max-w-lg mx-auto">
         <div className="space-y-2">
           {dummyData.map((todo) => (
-            <TodoItem todo={todo} />
+            <TodoItem 
+            key={todo.id}
+            todo={todo} 
+            onCompletedChange={setTodoCompleted}
+            
+            />
           ))}
         </div>
       </div>
